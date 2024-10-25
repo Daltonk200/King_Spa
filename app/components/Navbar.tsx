650cbb33b5eb210db9cd0c5fb378a40c6a1d7e15
Daltonk200 <mbengbright3@gmail.com>
2024-10-25 14:17:07 +0100

    trying to fixed delopment bug on navbar first try

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,37 +1,25 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-// import { UserButton, useAuth } from "@clerk/nextjs";
 import Image from "next/image";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const Navbar = () => {
-  // const { userId } = useAuth();
-  // const isAuth = !!userId;
   const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
-
+  
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-  
-    // Check if window is defined (only runs in the browser)
     if (typeof window !== "undefined") {
-      window.addEventListener("scroll", handleScroll);
-    }
+      const handleScroll = () => {
+        setScrolled(window.scrollY > 50);
+      };
   
-    return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("scroll", handleScroll);
-      }
-    };
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }
   }, []);
+  
+    
   
   return (
     <header>
@@ -55,37 +43,17 @@ const Navbar = () => {
           </Link>
         </div>
 
-        {/* <div className="lg:hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5">
-          <button onClick={toggleMenu}>
-            {isOpen ? (
-              <XMarkIcon className="w-4 h-5 ml-20 text-white" />
-            ) : (
-              <Bars3Icon className="w-9 h-10 text-white" />
-            )}
-          </button>
-        </div> */}
-
         <div
           className={`${
             isOpen ? "hidden" : "flex"
           } lg:flex lg:items-center lg:gap-4 z-20 mr-[-1.4rem]`}
         >
-          {/* {!isAuth ? ( */}
             <Link href="/appointment">
               <div className="text-white transition rounded bg-pink-500 hover:bg-pink-600 py-2 px-4 lg:py-3 lg:px-4 border-2">
                 Appointment
               </div>
             </Link>
-          {/* ) : (
-            <>
-              <Link href="/dashboard">
-                <div className="text-white transition rounded bg-pink-500 hover:bg-pink-600 py-2 px-4 lg:py-3 lg:px-4 border-2">
-                  Dashboard
-                </div>
-              </Link>
-              <UserButton afterSignOutUrl="/" />
-            </>
-          )} */}
+          
         </div>
       </nav>
     </header>
